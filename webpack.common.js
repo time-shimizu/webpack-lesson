b8e208020bc66bd3bea65808840daed1089f7916
time-shimizu <213.f.r48132@gmail.com>
2021-03-03 15:41:42 +0900

    複数のエントリーポイントを仕様

--- webpack.common.js
+++ webpack.common.js
@@ -3,15 +3,24 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/app.js',
+  entry: {
+    app: './src/js/app.js',
+    another: './src/js/another.js'
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'js/budle.js'
+    filename: 'js/[name].budle.js'
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/html/index.html'
+      template: './src/html/index.html',
+      chunks: ['app']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'another.html',
+      template: './src/html/another.html',
+      chunks: ['another']
     })
   ]
 }

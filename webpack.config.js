d49a9a5c57a5abd5a8094b0aeb4933f0b92d48b2
time-shimizu <213.f.r48132@gmail.com>
2021-03-03 12:14:38 +0900

    webpack.config.jsを追加

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = required('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'js/budle.js'
+  }
+}

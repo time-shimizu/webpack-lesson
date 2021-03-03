b15452530653ce845e19c930ef8ec90fa1829263
time-shimizu <213.f.r48132@gmail.com>
2021-03-03 14:43:08 +0900

    plugin,cleawebpackpluginを追加

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/js/app.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'js/budle.js'
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/*','!**.html']
+    })
+  ]
 }

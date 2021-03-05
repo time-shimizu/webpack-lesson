a00ac221874d735647594e5d1746298b47bf7312
time-shimizu <213.f.r48132@gmail.com>
2021-03-05 10:57:01 +0900

    prettierをloaderとして利用

--- src/js/another.js
+++ src/js/another.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import velocity from 'velocity-animate';
 import { greet } from './modules/greet';
 
-$('body').css('color', 'red')
-         .append(`<p>${greet('Another')}</p>`);
-velocity($('h1'), 'fadeIn', {duration: 2000, loop: true });
+$('body')
+  .css('color', 'red')
+  .append(`<p>${greet('Another')}</p>`);
+velocity($('h1'), 'fadeIn', { duration: 2000, loop: true });
--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,14 @@ import { greet } from './modules/greet';
 
 console.log('app');
 
-const result = add(1,2);
+const result = add(1, 2);
 
-$('body').append(result)
-         .append(`<p>${greet('App')}</p>`);
-velocity($('h1'), 'fadeIn', { duration: 2000, loop: true});
+$('body')
+  .append(result)
+  .append(`<p>${greet('App')}</p>`);
+velocity($('h1'), 'fadeIn', { duration: 2000, loop: true });
 
-const promise = new Promise((resolve) =>{
+const promise = new Promise((resolve) => {
   setTimeout(() => resolve('hello~'), 3000);
 });
 
